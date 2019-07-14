b38fec6abd31fb690d26eea0d7aa94bc01ab5758
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2019-07-14 23:45:58 +0400

    added minutes and hours counter

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -5,12 +5,43 @@ let seconds  = time.getSeconds();
 
 
 const secondsHand = document.querySelector('.second-hand')
+const minutesHand = document.querySelector('.min-hand')
+const hoursHand = document.querySelector('.hour-hand')
+
+seconds  = (seconds ) % 60 ;
+secondsDegree = ((seconds * 6) % 360) + 90;
+secondsHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+
+minutes  = (minutes) % 60 ;
+minutesDegree = ((minutes * 6) % 360) + 90;
+minutesHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+
+hours  = (hours) % 24 ;
+hoursHandDegree = ((hours * 15) % 360) + 90;
+hoursHand.style.transform = `rotate(${hoursHandDegree}deg)`;
 
 setInterval(function(){
 
     seconds  = (seconds + 1) % 60 ;
     secondsDegree = ((seconds * 6) % 360) + 90;
     secondsHand.style.transform = `rotate(${secondsDegree}deg)`;
-    console.log(seconds , secondsDegree)
 
 },1000)
+
+setInterval(function () { 
+
+    minutes  = (minutes + 1) % 60 ;
+    minutesDegree = ((minutes * 6) % 360) + 90;
+    minutesHand.style.transform = `rotate(${minutesDegree}deg)`;
+    console.log(minutes)
+},60000)
+
+setInterval(function () { 
+
+    hours  = (hours + 1) % 24 ;
+    hoursHandDegree = ((hours * 15) % 360) + 90;
+    hoursHand.style.transform = `rotate(${hoursHandDegree}deg)`;
+    console.log(hours)
+},3600000)
